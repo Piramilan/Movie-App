47dfc90e906298085adf20ec45c1d39af25df23b
Piramilan <65810943+Piramilan@users.noreply.github.com>
2022-07-06 21:42:41 +0530

    Add files via upload

--- src/api/MovieApi.js
+++ src/api/MovieApi.js
@@ -1,8 +1,10 @@
-import { API_KEY, BASE_URL } from "../constants/AppConstants";
+import { BASE_URL } from "../constants/AppConstants";
+
+const MOVIE_API_KEY = process.env.REACT_APP_API_MOVIE;
 
 export async function getTrendingMovies() {
   const request = await fetch(
-    `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&language=en-US`
+    `${BASE_URL}/trending/movie/week?api_key=${MOVIE_API_KEY}&language=en-US`
   ).then((res) => res.json());
 
   return request.results;
@@ -10,7 +12,7 @@ export async function getTrendingMovies() {
 
 export async function getUpComingMovies() {
   const request = await fetch(
-    `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`
+    `${BASE_URL}/movie/upcoming?api_key=${MOVIE_API_KEY}&language=en-US&page=1`
   ).then((res) => res.json());
 
   return request.results;
@@ -18,7 +20,7 @@ export async function getUpComingMovies() {
 
 export async function getMoviesGenre() {
   const request = await fetch(
-    `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=en-US`
+    `${BASE_URL}/genre/movie/list?api_key=${MOVIE_API_KEY}&language=en-US`
   ).then((res) => res.json());
 
   return request;
