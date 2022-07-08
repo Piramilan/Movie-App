a4cee0d895bf4f5e198a9f80e68bf06b85ec6b95
piramilan <piramilanshopify@gmail.com>
2022-07-08 21:16:34 +0530

    Stage1.1

--- src/modules/errors/Page404.jsx
+++ src/modules/errors/Page404.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { APP_ROUTES } from '../../constants/AppRoutes';
+import "./_Page404.scss"
 
 const Page404 = () => {
+
+  const navigate = useNavigate();
+
+  const changeRoute = (route) => {
+    navigate(`/${route}`);
+  };
+
   return (
-    <div>Page404</div>
+    <div className='not_found'>
+      <h1 className='error'>404</h1>
+      <p className='message'>Opps! The Page You Requested was not Found!</p>
+      <h4 className="back" onClick={() => changeRoute(APP_ROUTES.HOME)}>Back to Home</h4>
+      </div>
   )
 }
 
